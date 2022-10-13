b5cc915b9a439dcb874553c69ace7784a0bc97a0
Alberto <alberto-g@inventati.org>
2022-10-13 21:25:52 +0200

    Begin of porting of auto CW messages
    Does not compile yet

--- Core/Src/CWKeyer.c
+++ Core/Src/CWKeyer.c
@@ -214,4 +214,60 @@ void DoKeyer(void)
 		}
 	}
 }
+
+//Begin CWMessage
+
+const char m2c[] = "~ ETIANMSURWDKGOHVF*L*PJBXCYZQ**54S3***2**+***J16=/***H*7*G*8*90************?_****\"**.****@***'**-********;!*)*****,****:****";
+
+
+uint8_t delayWithKeySense(uint32_t ms){
+  uint32_t event = HAL_GetTick() + ms;
+  while(HAL_GetTick() < event){
+    if(KEYER_DASH || KEYER_DOT){
+      return 1;  // stop when button/key pressed
+    }
+  }
+  return 0;
+}
+
+char cw_msg[6][48] = { "CQ PE1NNN +", "CQ CQ DE PE1NNN PE1NNN +", "GE TKS 5NN 5NN NAME IS GUIDO GUIDO HW?", "FB RPTR TX 5W 5W ANT INV V 73 CUAGN", "73 TU E E", "PE1NNN" };
+
+
+uint8_t cw_msg_interval = 5; // number of seconds CW message is repeated
+uint32_t cw_msg_event = 0;
+uint8_t cw_msg_id = 0; // selected message
+
+int cw_tx(char ch){    // Transmit message in CW
+  char sym;
+  for(uint8_t j = 0; (sym = pgm_read_byte_near(m2c + j)); j++){  // lookup msg[i] in m2c, skip if not found
+    if(sym == ch){  // found -> transmit CW character j
+      uint8_t k = 0x80; for(; !(j & k); k >>= 1); k >>= 1; // shift start of cw code to MSB
+      if(k == 0) delay(ditTime * 4); // space -> add word space
+      else {
+        for(; k; k >>= 1){ // send dit/dah one by one, until everythng is sent
+          switch_rxtx(1);  // key-on  tx
+          if(delayWithKeySense(ditTime * ((j & k) ? 3 : 1))){ switch_rxtx(0); return 1; } // symbol: dah or dih length
+          switch_rxtx(0);  // key-off tx
+          if(delayWithKeySense(ditTime)) return 1;   // add symbol space
+        }
+        if(delayWithKeySense(ditTime * 2)) return 1; // add letter space
+      }
+      break; // next character
+    }
+  }
+  return 0;
+}
+
+int cw_tx(char* msg){
+  for(uint8_t i = 0; msg[i]; i++){  // loop over message
+    if(cw_tx(msg[i])) return 1;
+  }
+  return 0;
+}
+
+
+void SendCWMessage(uint8_t MessageNo)
+{
+
+}
 //#pragma GCC pop_options
