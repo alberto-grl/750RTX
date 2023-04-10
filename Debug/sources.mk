00a68dfa35c3f54b74c28a34ecb8d610f27777fc
Alberto <alberto-g@inventati.org>
2023-04-10 23:26:44 +0200

    Moved CMSIS dir

--- Debug/sources.mk
+++ Debug/sources.mk
@@ -22,8 +22,8 @@ C_DEPS :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-Core/CMSIS \
 Core \
+Core/Src/CMSIS \
 Core/Src \
 Core/Startup \
 Drivers/STM32H7xx_HAL_Driver/Src \
